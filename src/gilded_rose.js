88dbfdc9571a944da13babf3a789eff99b62a3dd
Daniyal Amjed <danial@vidivet.com>
2024-01-17 03:23:13 +0500

    combing if together

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,10 +18,8 @@ function updateAged(item) {
   }
   item.sellIn = item.sellIn - 1;
 
-  if (item.sellIn < 0) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
+  if (item.sellIn < 0 && item.quality < 50) {
+    item.quality = item.quality + 1;
   }
 }
 
