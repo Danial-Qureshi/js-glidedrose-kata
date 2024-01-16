e0bfb3ed09ce301c03eaf90377779fea982a74a5
Daniyal Amjed <danial@vidivet.com>
2024-01-17 03:46:56 +0500

    adding test file

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -1,11 +1,35 @@
-var {expect} = require('chai');
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+var { expect } = require("chai");
+var { Shop, Item } = require("../src/gilded_rose.js");
+describe("Gilded Rose", function () {
+  it("should foo", function () {
+    const names = [
+      "Aged Brie",
+      "Backstage passes to a TAFKAL80ETC concert",
+      "Sulfuras, Hand of Ragnaros",
+      "normal item",
+    ];
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    const tests = [];
+
+    const [minSellIn, maxSellIn] = [-1, 12];
+    const [minQuality, maxQuality] = [-1, 51];
+
+    for (const name of names) {
+      for (let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++) {
+        for (let quality = minQuality; quality <= maxQuality; quality++) {
+          const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+          const items = gildedRose.updateQuality();
+
+          const outputSellIn = items[0].sellIn;
+          const outputQuality = items[0].quality;
+
+          tests.push([name, sellIn, quality, outputSellIn, outputQuality]);
+        }
+      }
+    }
+
+    console.log(JSON.stringify(tests));
+
+    console.log(tests.length);
   });
-
 });
